c25aebb44d2d2455aca7b518036ce69bdceae14e
DJ SP <DJSP@MacBook-Pro-DJ.local>
2018-05-19 20:08:59 +0300

    стили для слайдера

---
+++ js/sliders.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#best-team-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        mobileFirst: true,
+        speed: 1100,
+        appendDots: $('.section.best-team')
+
+    });
+
+
+    $('#clients-says-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        mobileFirst: true,
+        speed: 1100
+    }
+    );
+});
+
